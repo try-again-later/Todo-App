3a627f8a3581d4c4451719cd2532c9b7f0b71fdb
try-again-later <aa.popok@yandex.ru>
2022-06-27 02:35:56 +0300

    Add `AppPaths` class

---
+++ app/AppPaths.php
@@ -0,0 +1,35 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace TryAgainLater\TodoApp;
+
+use TryAgainLater\TodoApp\Util\Path;
+
+class AppPaths
+{
+    public function __construct(private string $rootPath)
+    {
+        assert(str_ends_with($rootPath, DIRECTORY_SEPARATOR));
+    }
+
+    public function root(string ...$segments): string
+    {
+        return Path::appendFile($this->rootPath, ...$segments);
+    }
+
+    public function storage(string ...$segments): string
+    {
+        return $this->root('storage', ...$segments);
+    }
+
+    public function public(string ...$segments): string
+    {
+        return $this->root('public', ...$segments);
+    }
+
+    public function errorLog(): string
+    {
+        return self::storage('logs', 'error.log');
+    }
+}
---
+++ app/Util/Path.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace TryAgainLater\TodoApp\Util;
+
+class Path
+{
+    static function joinSegments(string ...$segments)
+    {
+        return implode(DIRECTORY_SEPARATOR, $segments);
+    }
+
+    static function appendFile(string $folderPath, string ...$segments)
+    {
+        assert(str_ends_with($folderPath, DIRECTORY_SEPARATOR));
+
+        return $folderPath . self::joinSegments(...$segments);
+    }
+
+    static function appendFolder(string $folderPath, string ...$segments)
+    {
+        assert(str_ends_with($folderPath, DIRECTORY_SEPARATOR));
+
+        if (count($segments) === 0) {
+            return $folderPath;
+        }
+
+        return self::appendFile($folderPath, ...$segments) . DIRECTORY_SEPARATOR;
+    }
+}
